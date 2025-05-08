9f7e9036da2ca7673e84b42d8a79a886e9a5693d
Tiago Novais <tiago.novais@tecnico.ulisboa.pt>
2025-05-08 23:13:12 +0100

    Projeto
    Leitura do input e teste com uma instância de exemplo (formato de entrada)

---
+++ proj2425base-nuruomino/nuruomino.py
@@ -0,0 +1,111 @@
+# nuruomino.py: Template para implementação do projeto de Inteligência Artificial 2024/2025.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes sugeridas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 31:
+# 84888 Tiago Novais
+
+from sys import stdin
+
+class NuruominoState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = Nuroumino.state_id
+        Nuroumino.state_id += 1
+
+    def __lt__(self, other):
+        """ Este método é utilizado em caso de empate na gestão da lista
+        de abertos nas procuras informadas. """
+        return self.id < other.id
+
+class Board:
+    """Representação interna de um tabuleiro do Puzzle Nuruomino."""
+
+    def __init__(self, gameBoard: dict):
+        self.gameBoard = gameBoard
+
+    def adjacent_regions(self, region:int) -> list:
+        """Devolve uma lista das regiões que fazem fronteira com a região enviada no argumento."""
+        #TODO
+        pass
+    
+    def adjacent_positions(self, row:int, col:int) -> list:
+        """Devolve as posições adjacentes à região, em todas as direções, incluindo diagonais."""
+        #TODO
+        pass
+
+    def adjacent_values(self, row:int, col:int) -> list:
+        """Devolve os valores das celulas adjacentes à região, em todas as direções, incluindo diagonais."""
+        #TODO
+        pass
+    
+    def display_board(self):
+        """Imprime o tabuleiro."""
+        
+        board = ""      
+        maxRow = max(key[0] for key in self.gameBoard.keys())
+        maxCol = max(key[1] for key in self.gameBoard.keys())
+        separator = ""
+
+        for row in range(1, maxRow + 1):
+            for col in range(1, maxCol + 1):
+                board += separator + str(self.gameBoard[(row, col)])
+                separator = " "
+            separator = "" 
+            board += "\n"
+        return print(board)   
+        
+    @staticmethod
+    def parse_instance():
+        """Leitura do Board apartir do ficheiro."""
+        
+        gameBoard = {}
+        matrixLine = 1
+        matrixColumn = 1
+        
+        for line in stdin:
+            line = line.split()
+            for value in range(len(line)):
+                gameBoard[(matrixLine, matrixColumn)] = int(line[value]) 
+                matrixColumn += 1              
+            matrixColumn = 1
+            matrixLine += 1
+        return Board(gameBoard)  
+    
+    # TODO: outros metodos da classe Board
+
+class Nuruomino(Problem):
+    def __init__(self, board: Board):
+        """O construtor especifica o estado inicial."""
+        #TODO
+        pass 
+
+    def actions(self, state: NuruominoState):
+        """Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento."""
+        #TODO
+        pass 
+
+    def result(self, state: NuruominoState, action):
+        """Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de
+        self.actions(state)."""
+
+        #TODO
+        pass 
+        
+
+    def goal_test(self, state: NuruominoState):
+        """Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro
+        estão preenchidas de acordo com as regras do problema."""
+        #TODO
+        pass 
+
+    def h(self, node: Node):
+        """Função heuristica utilizada para a procura A*."""
+        # TODO
+        pass
